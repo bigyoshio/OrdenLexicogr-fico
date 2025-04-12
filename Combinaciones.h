50f36a6a5e2c84ed1d5f614f7a9b4314fef4890b
bigyoshio <albertogalvezmendoza00@gmail.com>
2025-04-11 18:12:06 -0700

    Update Combinaciones.h

--- Combinaciones.h
+++ Combinaciones.h
@@ -1,12 +1,16 @@
-#define GESTIONES_H_INCLUDED
+#ifndef COMBINACIONES_H_INCLUDED
+#define COMBINACIONES_H_INCLUDED
 
-#include <iostream>
-#include <fstream>
-#include <string>
+/**
+ * @brief Calcula el número de combinaciones posibles de un conjunto
+ * 
+ * @param totalElementos Número total de elementos en el conjunto (n)
+ * @param tamanoSubconjunto Tamaño de cada subconjunto (r)
+ * @return long double Número total de combinaciones C(n,r) = n!/(r!(n-r)!)
+ * 
+ * @note Esta función usa un método iterativo para evitar desbordamientos con factoriales grandes
+ * @note Retorna 0 si r > n, 1 si r == 0 o r == n
+ */
+long double calcularCombinaciones(int totalElementos, int tamanoSubconjunto);
 
-void procesarCombinacion(const int* combinacionActual, int tamanoSubconjunto, 
-                         std::ofstream& archivoSalida, int& contadorCombinaciones);
-void generarCombinaciones(int totalElementos, int& contadorCombinaciones, 
-                         int tamanoSubconjunto, const std::string& nombreArchivo = "");
-
-#endif // GESTIONES_H_INCLUDED
+#endif // COMBINACIONES_H_INCLUDED
