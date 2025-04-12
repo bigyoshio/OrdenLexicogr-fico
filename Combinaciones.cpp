c17696c212aea3f5a5fe550cc5085a41719591fa
bigyoshio <albertogalvezmendoza00@gmail.com>
2025-04-11 18:10:03 -0700

    Update Combinaciones.cpp

--- Combinaciones.cpp
+++ Combinaciones.cpp
@@ -2,16 +2,13 @@
 #include <iostream>
 #include "Combinaciones.h"
 
-
-
-long double calcularCombinaciones(int n, int r) {
-    if (r > n) return 0;
-    if (r == 0 || r == n) return 1;
+long double calcularCombinaciones(int totalElementos, int tamanoSubconjunto) {
+    if (tamanoSubconjunto > totalElementos) return 0;
+    if (tamanoSubconjunto == 0 || tamanoSubconjunto == totalElementos) return 1;
 
     long double resultado = 1;
-    for (int i = 1; i <= r; ++i) {
-        resultado = resultado * (n - r + i) / i;
+    for (int iterador = 1; iterador <= tamanoSubconjunto; ++iterador) {
+        resultado = resultado * (totalElementos - tamanoSubconjunto + iterador) / iterador;
     }
     return resultado;
 }
-
