ba674aec21f707cb9ccf1de7b6d59942c62328ef
bigyoshio <albertogalvezmendoza00@gmail.com>
2025-04-11 18:11:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,32 +3,33 @@
 #include "Gestiones.h"
 #include "Combinaciones.h"
 
-int main()
-{
-    long double combinacionesTeoricas;
-    int n, r;
+int main() {
+    long double numeroCombinacionesTeoricas;
+    int totalElementos, tamanoSubconjunto;
 
     std::cout << "GENERADOR DE COMBINACIONES EN ORDEN LEXICOGRAFICO\n";
 
     std::cout << "Ingrese el total de elementos (n) con n >= 1: ";
-    std::cin >> n;
+    std::cin >> totalElementos;
 
-    std::cout << "Ingrese el tamaño de la combinacion (r) con r <= n: ";
-    std::cin >> r;
+    std::cout << "Ingrese el tamaÅ„o de la combinacion (r) con r <= n: ";
+    std::cin >> tamanoSubconjunto;
 
-    if (n < 1 || r < 0 || r > n) {
-        std::cerr << "Error: Valores inválidos para n y r" << std::endl;
+    if (totalElementos < 1 || tamanoSubconjunto < 0 || tamanoSubconjunto > totalElementos) {
+        std::cerr << "Error: Valores invÃ¡lidos para totalElementos y tamanoSubconjunto" << std::endl;
         return 1;
     }
 
-    std::cout << "\nEl conjunto se generara con numeros de 1 a " << n << "\n";
-    std::cout << "\nGenerando combinaciones de un conjunto de tamaño " << n << " en cadenas de tamaño " << r << ":\n";
+    std::cout << "\nEl conjunto se generara con numeros de 1 a " << totalElementos << "\n";
+    std::cout << "\nGenerando combinaciones de un conjunto de tamaÅ„o " << totalElementos 
+              << " en cadenas de tamaÅ„o " << tamanoSubconjunto << ":\n";
 
-    combinacionesTeoricas = calcularCombinaciones(n, r);
-    std::cout << "\nTeoricamente el numero de combinaciones deberia ser: " << combinacionesTeoricas << "\n";
+    numeroCombinacionesTeoricas = calcularCombinaciones(totalElementos, tamanoSubconjunto);
+    std::cout << "\nTeoricamente el numero de combinaciones deberia ser: " 
+              << numeroCombinacionesTeoricas << "\n";
 
     int combinacionesGeneradas = 0;
-    generarCombinaciones(n, combinacionesGeneradas, r);
+    generarCombinaciones(totalElementos, combinacionesGeneradas, tamanoSubconjunto);
     std::cout << "\nEl total de combinaciones generadas fue: " << combinacionesGeneradas << "\n";
 
     return 0;
