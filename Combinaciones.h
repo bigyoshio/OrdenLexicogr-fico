7b0438c6468d7ddf5c58920f3594c517ecff1280
bigyoshio <albertogalvezmendoza00@gmail.com>
2025-04-11 17:38:35 -0700

    Update Combinaciones.h

--- Combinaciones.h
+++ Combinaciones.h
@@ -1,6 +1,15 @@
 #ifndef COMBINACIONES_H_INCLUDED
 #define COMBINACIONES_H_INCLUDED
 
+/**
+ * @brief Calcula el número de combinaciones posibles (nCr)
+ * @param n Número total de elementos
+ * @param r Tamaño de cada combinación
+ * @return Número de combinaciones como long double
+ * @note Retorna 0 si r > n
+ * @note Retorna 1 si r == 0 o r == n
+ * @note Usa un método iterativo para evitar overflow numérico
+ */
 long double calcularCombinaciones(int n, int r);
 
 #endif // COMBINACIONES_H_INCLUDED
